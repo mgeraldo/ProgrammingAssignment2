8eb85875933930c4efa77f3690b9dc76c4c6d82d
mgeraldo <marcos.geraldo@gmail.com>
2014-12-26 21:24:07 -0800

    this is the submission of the cachematrix file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Prorgamming Assignment 2: inverting a matrix or capturing the value calculated previously. 
 
-## Write a short comment describing this function
+## This is just an example of an invertible matrix
+x <- matrix(c(2,rep(0,4),2,rep(0,4),1,rep(0,4),1),4,4)
+
+# makeCacheMatrix cleans the inverted matrix in case that x is redefined
+# to start this function must be called as follows:  
+# > makeCacheMatrix(x)
+# or
+# > x <- makeCacheMatrix(x)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+mat <<- x
+inv <<- NULL
+invisible(x)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve check whether the inverse martix has been calculated and either brings the 
+## value stored in cache, or inverts the matrix updating the cache.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x) {
         ## Return a matrix that is the inverse of 'x'
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    inv <<- solve(x)
+    inv
 }
